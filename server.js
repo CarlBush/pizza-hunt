48985a5292648efec437136bcc70f376dcc67541
Carl Bush <CarlBushs13@gmail.com>
2022-05-22 09:44:44 -0700

    model index.js and create mongoose.connect

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const express = require('express');
 
 const app = express();
@@ -8,5 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+
+//mongoose.connect needs to be before the app.listen
+//IF environment variable MONGODB_UR exists (like on heroku) use that OR use local mongo server database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set("debug", true);
+
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
